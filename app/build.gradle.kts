bd0cbe0db876a1d058880e325f1bb1030352d3ee
Razin <root@razin.me>
2026-06-21 15:10:06 +0000

    app: generate x86_64 APKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -166,7 +166,7 @@ configure<ApplicationExtension> {
             isEnable = !isBuildingBundle.get()
 
             reset()
-            include("armeabi-v7a", "arm64-v8a")
+            include("armeabi-v7a", "arm64-v8a", "x86_64")
             isUniversalApk = true
         }
     }
